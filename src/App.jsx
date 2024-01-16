aebece1715ae689b70820751bf116ca8aaf5171a
Bryan Gallo <bryangallo95@hotmail.com>
2024-01-16 05:03:54 -0500

    Iniciando CotizadorAuto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        
+            <div>
+                <h1>Cotizador de autos</h1>
+            </div>
+    );
+}
+
+export default App;
