2f5522ace9a8a3e8770fab6ccfe455f71ea83a47
Bryan Gallo <bryangallo95@hotmail.com>
2024-02-21 06:07:06 -0500

    Mostrando nombre Marca-Plan y resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,19 +1,40 @@
 import useCotizador from "../hooks/useCotizador";
+import { MARCAS, PLANES } from "../constants";
+
 const Resultado = () => {
     const { resultado, datos } = useCotizador();
     const { marca, year, plan } = datos;
+
+    const [nombreMarca] = MARCAS.filter((m) => m.id === Number(marca));
+
+    const [nombrePlan] = PLANES.filter((p) => p.id === Number(plan));
+
     if (resultado === 0) {
         return null;
     }
     return (
         <div className="bg-gray-100 text-center mt-5 p-5 shadow">
             {" "}
-            <h2 className="text-gray-500 font-black text-2xl">
-                El total es: {resultado}
-            </h2>
-            <p className="text-gray-500 font-bold m-2">
+            <h2 className="text-gray-600 font-black text-3xl">Resumen</h2>
+            <p className="m-2">
+                <span className="font-bold"> El total es: </span>
+                {resultado}
+            </p>
+            <p className="m-2">
                 <span className="font-bold">Marca: </span>
-                {marca}
+                {nombreMarca.nombre}
+            </p>
+            <p className="m-2">
+                <span className="font-bold">Plan: </span>
+                {nombrePlan.nombre}
+            </p>
+            <p className="m-2">
+                <span className="font-bold">Año del auto: </span>
+                {year}
+            </p>
+            <p className="m-2 text-2xl">
+                <span className="font-bold">Total Cotización: </span>
+                {resultado}
             </p>
         </div>
     );
